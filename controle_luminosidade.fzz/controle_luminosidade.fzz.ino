683cb7e92809c7533c9eac587cc659ecd177601d
Rodrigo Lessa <rodrigolsr@gmail.com>
2018-09-09 12:21:11 -0300

    Incluindo javino

--- controle_luminosidade.fzz/controle_luminosidade.fzz.ino
+++ controle_luminosidade.fzz/controle_luminosidade.fzz.ino
@@ -1,9 +1,14 @@
+include <Javino.h>
+Javino j;
+
 int pinoLDR = A0; // pino de entrada do sensor de limunosidade
 int pinoLED = 2; // pino de saída do LED vermelho
-int lum = 0; // armazenar o valor lido do LDR
-int i = 0;
 int pinoTemp = A1; // pino de entrada do sensor de temperatura
-int temp = 20; // temperatura inicial
+int lum = 0; // armazenar o valor lido do LDR
+int temp = 20; // temperatura inicial em ºC
+int tempWarning = 40; // temperatura superior ao normal
+int tempDanger = 60; // temperatura de incêndio
+int i = 0;
 
 void setup() {
   pinMode(pinoLDR, INPUT); // declarando pino de entrada
@@ -12,34 +17,45 @@ void setup() {
 }
 
 void loop() {
+  if (j.availablemsg()){
+    actions(j.getmsg());
+  }
 
+  //delay(1200); // Aguardar x millisegundos até a póxima verificação
+}
+
+void actions(String msg){
+  
   // Verificando luminosidade
-
-  lum = 0;
+  if (msg == "readLum"){
+    
+    lum = 0;
+    
+    for (i = 0; i < 3; i = i + 1){
+      lum = lum + analogRead(pinoLDR); // lendo o sensor
+      delay(200);
+    }
   
-  for (i = 0; i < 3; i = i + 1){
-    lum = lum + analogRead(pinoLDR); // lendo o sensor
-    delay(200);
-  }
-
-  lum = lum / 3;
+    lum = lum / 3;
+    
+    Serial.println(lum); // exibindo no console a resistencia do sensor LDR
   
-  Serial.println(lum); // exibindo no console a resistencia do sensor LDR
-
-  if (lum > 800){
-    digitalWrite(pinoLED, LOW); // muito claro, desligar o LED
-  }
-  if (lum < 400){
-    digitalWrite(pinoLED, HIGH); // ligar o LED
+    if (lum > 800){
+      digitalWrite(pinoLED, LOW); // muito claro, desligar o LED
+    }
+    if (lum < 400){
+      digitalWrite(pinoLED, HIGH); // ligar o LED
+    }
   }
 
   // Verificando temperatura
+  if (msg == "readLum"){
 
-  temp = analogRead(pinoTemp); // lendo temperatura
-  temp = temp * 0.488; // convertendo °C 
-
-  Serial.print(temp);
-  Serial.println(" °C");  // exbindo somente para testes
- 
-  delay(1200); // Aguardar x millisegundos até a póxima verificação
+    temp = analogRead(pinoTemp); // lendo temperatura
+    temp = temp * 0.488; // convertendo °C 
+  
+    Serial.print(temp);
+    //Serial.println(" °C");  // exbindo somente para testes
+  }
+  
 }
